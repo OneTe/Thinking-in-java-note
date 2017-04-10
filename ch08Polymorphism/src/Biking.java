10b4f65e011d8c4ae28ac7c177d3ef31382b51c5
wangcheng <veoxft@hotmail.com>
2017-04-10 09:58:48 +0800

    向上转型

---
+++ ch08Polymorphism/src/Biking.java
@@ -0,0 +1,20 @@
+/**
+ * Created by wangcheng on 2017/4/7.
+ */
+class Cycle{
+    public static void ride(Cycle c){
+        System.out.println("Cycle.ride()" + c);
+    }
+
+}
+class Unicycle extends Cycle{
+    
+}
+class Bicycle extends Cycle{
+
+}
+class Tricycle extends Cycle{
+
+}
+public class Biking {
+}
