c512f083525df378d74bc3deb66e7188aedbdacb
wangcheng <veoxft@hotmail.com>
2017-05-08 18:16:07 +0800

    通过继承来扩展接口

---
+++ ch09-21/src/Interfaces/DiamondInheritance13.java
@@ -0,0 +1,36 @@
+package Interfaces;
+
+// TIJ4 Chapter Interfaces, Exercise 13, page 328
+/* Create an interface, and inherit two new interfaces from that
+* interface. Multiply inherit a third interface from the second
+* two.
+*/
+/**
+ * Created by wangcheng on 2017/5/3.
+ */
+interface CanDo{
+    void doIt();
+}
+interface CanDoMore extends CanDo{
+    void doMore();
+}
+interface CanDoFaster extends CanDo{
+    void doFaster();
+}
+interface CanDoMost extends CanDoMore,CanDoFaster{
+    void doMost();
+}
+class Doer implements CanDoMost{
+    public void doIt(){}
+    public void doMore(){}
+    public void doFaster(){}
+    public void doMost(){}
+}
+public class DiamondInheritance13 {
+    public static void main(String[] args){
+        Doer d = new Doer();
+        ((CanDoMore)d).doMore();
+        ((CanDoFaster)d).doFaster();
+        ((CanDoMost)d).doMost();
+    }
+}
