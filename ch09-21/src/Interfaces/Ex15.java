a2c5509737c33851ad6693c2cb06ee5abce6b038
wangcheng <veoxft@hotmail.com>
2017-05-11 21:50:59 +0800

    适配接口

---
+++ ch09-21/src/Interfaces/Ex15.java
@@ -0,0 +1,22 @@
+package Interfaces;
+
+import java.util.Arrays;
+
+/**
+ * Created by wangcheng on 2017/5/9.
+ */
+public class Ex15 {
+    public static void main(String[] args) {
+        int[] intArray = {10, 2, 5, 23,4};
+        for (int i = 0; i < intArray.length-1; i++) {
+            for(int j = 0;j<intArray.length-1-i;j++) {
+                if (intArray[j] > intArray[j + 1]) {
+                    int temp = intArray[j];
+                    intArray[j] = intArray[j + 1];
+                    intArray[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(intArray));
+    }
+}
