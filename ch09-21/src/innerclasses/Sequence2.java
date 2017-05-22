934fed30aaa2dd2e49ed904562c7a8e6e9e5e236
wangcheng <veoxft@hotmail.com>
2017-05-22 15:10:48 +0800

    Exercise innerClass

---
+++ ch09-21/src/innerclasses/Sequence2.java
@@ -0,0 +1,63 @@
+package innerclasses;
+// TIJ4 Chapter Innerclasses, Exercise 2, page 350
+/* Create a class that holds a String, and has a toString() method that
+* displays this String. Add several instances of your new class to a
+* Sequence ojbect, then display them.
+*/
+import java.nio.channels.*;
+
+/**
+ * Created by wangcheng on 2017/5/22.
+ */
+class Word{
+    private String word;
+    public Word(String s){word = s;}
+    public String toString(){return word;}
+}
+interface Selector2{
+    boolean end();
+    Object current();
+    void next();
+}
+public class Sequence2 {
+    private Object[] items;
+    private int next = 0;
+    public Sequence2(int size){items = new Object[size];}
+    public void add(Object x){
+        if(next<items.length){
+            items[next++] = x;
+        }
+    }
+    private class SequenceSelector implements Selector2{
+        private int i = 0;
+        public boolean end(){return i == items.length;}
+        public Object current(){return items[i];}
+        public void next(){if(i<items.length) i++;}
+    }
+    public Selector2 selector(){
+        return new SequenceSelector();
+    }
+    public static void main(String[] args){
+        Sequence2 sequence = new Sequence2(10);
+        for(int i = 0;i<10;i++){
+            sequence.add(new Word(Integer.toString(i)));
+        }
+        Selector2 selector = sequence.selector();
+        while(!selector.end()){
+            System.out.println(selector.current() + " ");
+            selector.next();
+        }
+        Word w1 = new Word("Peace");
+        Word w2 = new Word("Love");
+        Word w3 = new Word("Easter");
+        Sequence2 message = new Sequence2(3);
+        message.add(w1);
+        message.add(w2);
+        message.add(w3);
+        Selector2 sel = message.selector();
+        while(!sel.end()){
+            System.out.println(sel.current() + " ");
+            sel.next();
+        }
+    }
+}
