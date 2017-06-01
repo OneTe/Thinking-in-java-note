fa3a2dfce87af4910fe9e179cb08dd55690af62a
wangcheng <veoxft@hotmail.com>
2017-06-01 17:39:56 +0800

    exercise

---
+++ ch09-21/src/innerclasses/Outer7.java
@@ -0,0 +1,33 @@
+package innerclasses;
+// TIJ4 Chapter Innerclasses, Exercise 7, page 354
+/* Create a class with a private field and a private method. Create an
+* inner class with a method that modifies the outer-class field and calls
+* the outer class method. In a second outer-class method, create an object
+* of the inner class and call its method, then show the effect on the
+* outer-class object.
+*/
+/**
+ * Created by wangcheng on 2017/6/1.
+ */
+public class Outer7 {
+    private int oi = 7;
+    private void hi(){System.out.println("Outer7 hi()");}
+    class Inner{
+        void modifyOuter(){
+            oi *= 2;
+            hi();
+        }
+        private int xx =4;
+    }
+    public void showOi(){System.out.println(oi);}
+    void testInner(){
+        Inner in = new Inner();
+        in.modifyOuter();
+    }
+    public static void main(String[] args){
+        Outer7 out = new Outer7();
+        out.showOi();
+        out.testInner();
+        out.showOi();
+    }
+}
