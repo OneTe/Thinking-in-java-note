4d885bed1bcaa10dd235890894a90537f085eeb1
wangcheng <veoxft@hotmail.com>
2017-06-05 18:11:39 +0800

    exercise innerclass which define in a method or a scope

---
+++ ch09-21/src/innerclasses/Ex10.java
@@ -0,0 +1,27 @@
+package innerclasses;
+/* Repeat the previous exercise but define the inner class within a
+* scope with scope within a method.
+*/
+/**
+ * Created by wangcheng on 2017/6/5.
+ */
+interface  Ex10Interface{
+    void say(String s);
+}
+public class Ex10 {
+    Ex10Interface f(boolean b){
+        if(b){
+            class Inner implements Ex10Interface{
+                public void say(String s){
+                    System.out.println(s);
+                }
+            }
+            return new Inner();
+        }
+        return null;
+    }
+    public static void main(String[] args){
+        Ex10 x = new Ex10();
+        x.f(true).say("hi!");
+    }
+}
