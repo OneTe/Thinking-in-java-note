88a7014bbc1339f26ea2826f64d1ace0826517a1
wangcheng <veoxft@hotmail.com>
2017-08-09 22:01:46 +0800

    Map exercise

--- ch09-21/src/holding/Ex25.java
+++ ch09-21/src/holding/Ex25.java
@@ -1,5 +1,14 @@
 package holding;
 
+// TIJ4 Chapter Holding, Exercise 25, page 423
+/* Create a Map<String, ArrayList<Integer>>. Use net.mindview.TextFile
+* to open a text file and read it in a word at a time (use "\\W+\" as
+* the second argument to the TextFile constructor). Count the words as
+* you read them in, and for each word in the file, record in the
+* ArrayList<Integer> the word count associated with that word - that is,
+* in effect, the location in the file where that word was found.
+*/
+
 /**
  * Created by wangcheng on 2017/8/8.
  */
@@ -7,6 +16,24 @@ import java.util.*;
 
 public class Ex25 {
     public static void main(String[] args){
-
+        Map<String,ArrayList<Integer>> m = new LinkedHashMap<>();
+        List<String> words = new LinkedList<>();
+        words.addAll(new TextFile("src/holding/SetOperations.java","\\W+"));
+        System.out.println("Words in file: " + words);
+        Iterator<String> itWords = words.iterator();
+        int count = 0;
+        while (itWords.hasNext()){
+            String s = itWords.next();
+            count ++;
+            if(!m.keySet().contains(s)){
+                ArrayList<Integer> ai = new ArrayList<>();
+                ai.add(0,count);
+                m.put(s,ai);
+            }else {
+                m.get(s).add(count);
+                m.put(s,m.get(s));
+            }
+        }
+        System.out.println("Map of word locations: " + m);
     }
 }
---
+++ ch09-21/src/holding/Ex26.java
@@ -0,0 +1,52 @@
+package holding;
+// holding/Ex26.java
+// TIJ4 Chapter Holding, Exercise 26, page 423
+/* Take the resulting Map from the previous exercise and re-create the
+* order of the words as they appeared in the original file.
+*/
+/**
+ * Created by wangcheng on 2017/8/9.
+ */
+import java.util.*;
+
+public class Ex26 {
+    public static void main(String[] args){
+        Map<String,ArrayList<Integer>> m =
+                new LinkedHashMap<String,ArrayList<Integer>>();
+        List<String> words = new LinkedList<String>();
+        words.addAll(new TextFile("src/holding/SetOperations.java", "\\W+"));
+        System.out.println("Words in file: " + words);
+        Iterator itWords = words.iterator();
+        int count = 0;
+        while(itWords.hasNext()) {
+            String s = (String)itWords.next();
+            count++;
+            if(!m.keySet().contains(s)) {
+                ArrayList<Integer> ai =
+                        new ArrayList<Integer>();
+                ai.add(0, count);
+                m.put(s, ai);
+            }
+            else {
+                m.get(s).add(count);
+                m.put(s, m.get(s));
+            }
+        }
+        System.out.println();
+        System.out.println("Map of word locations: " + m);
+        // New Map to hold sorted words, keyed by location:
+        Map<Integer,String> replay = new TreeMap<>();
+        Iterator<Map.Entry<String,ArrayList<Integer>>> it = m.entrySet().iterator();
+        while (it.hasNext()){
+            Map.Entry<String,ArrayList<Integer>> me = it.next();
+            for(int i =0;i<me.getValue().size();i++){
+                replay.put(me.getValue().get(i),me.getKey());
+            }
+        }
+        System.out.println();
+        System.out.println("TreeMap of ordered locations, words: " + replay);
+        // Display words in order as TreeMap values():
+        System.out.println("Words in original order: " +
+                replay.values());
+    }
+}
