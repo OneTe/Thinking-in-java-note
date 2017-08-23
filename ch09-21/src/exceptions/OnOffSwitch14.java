047d3329e970bae005eed0a8380110013444ac57
wangcheng <veoxft@hotmail.com>
2017-08-23 17:31:56 +0800

    finally excrcise

---
+++ ch09-21/src/exceptions/OnOffSwitch14.java
@@ -0,0 +1,26 @@
+package exceptions;
+// TIJ4 Chapter Exceptions, Exercise 14, page 476
+// Show that the OnOffSwitch.java can fail by throwing a
+// RuntimeException inside the try block.
+/**
+ * Created by wangcheng on 2017/8/23.
+ */
+public class OnOffSwitch14 {
+    private static Switch sw  = new Switch();
+    static Integer[] x = new Integer[1];
+    public static void f(int i)
+        throws OnOffException1,OnOffException2{}
+    public static void main(String[] args){
+        try{
+            sw.on();
+            f(x[0]);
+            sw.off();
+        }catch (OnOffException1 e){
+            System.out.println("OnOffException1");
+            sw.off();
+        }catch (OnOffException2 e){
+            System.out.println("OnOffException2");
+            sw.off();
+        }
+    }
+}
