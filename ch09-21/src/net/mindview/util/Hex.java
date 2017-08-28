a3fc1f47fba64fdc55cc0b731e5f644661883a9e
wangcheng <veoxft@hotmail.com>
2017-08-28 17:12:30 +0800

    一个十六进制dump工具

---
+++ ch09-21/src/net/mindview/util/Hex.java
@@ -0,0 +1,34 @@
+package net.mindview.util;
+
+/**
+ * Created by wangcheng  on 2017/8/28.
+ */
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.io.*;
+
+public class Hex {
+    public static String format(byte[] data){
+        StringBuilder result = new StringBuilder();
+        int n = 0;
+        for(byte b:data){
+            if(n %  16 == 0){
+                result.append(String.format("%05X: ",n));
+            }
+            result.append(String.format("%02X ",b));
+            n++;
+            if(n % 16 == 0){
+                result.append("\n");
+            }
+        }
+        result.append("\n");
+        return result.toString();
+    }
+    public static void main(String[] args) throws Exception{
+        if(args.length == 0){
+            System.out.println(format(BinaryFile.read("src/net/mindview/util/Hex.java")));
+        }else {
+            System.out.println(format(BinaryFile.read(args[0])));
+        }
+    }
+}
