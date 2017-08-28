41301a65af2e12067fbb1ece471c793d3d6dddb5
wangcheng <veoxft@hotmail.com>
2017-08-28 17:18:13 +0800

    String.format()

---
+++ ch09-21/src/strings/DatabaseException.java
@@ -0,0 +1,17 @@
+package strings;
+
+/**
+ * Created by wangcheng  on 2017/8/28.
+ */
+public class DatabaseException extends Exception{
+    public DatabaseException(int transactionID,int queryID,String message){
+        super(String.format("(t%d,q%d) %s",transactionID,queryID,message));
+    }
+    public static void main(String[] args){
+        try {
+            throw new DatabaseException(3,7,"Write failed");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
