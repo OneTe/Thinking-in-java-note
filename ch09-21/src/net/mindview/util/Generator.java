ac4208cebd8b8476e653d984be88e0d49d56bb43
wangcheng <veoxft@hotmail.com>
2017-09-04 17:06:15 +0800

    Map工具类

---
+++ ch09-21/src/net/mindview/util/Generator.java
@@ -0,0 +1,8 @@
+package net.mindview.util;
+
+/**
+ * Created by wangcheng  on 2017/9/4.
+ */
+//: net/mindview/util/Generator.java
+// A generic interface.
+public interface Generator<T> { T next(); } ///:~
---
+++ ch09-21/src/net/mindview/util/Pair.java
@@ -0,0 +1,13 @@
+package net.mindview.util;
+
+/**
+ * Created by wangcheng  on 2017/9/4.
+ */
+public class Pair<K,V> {
+    public final K key;
+    public final V value;
+    public Pair(K k, V v) {
+        key = k;
+        value = v;
+    }
+} ///:~
