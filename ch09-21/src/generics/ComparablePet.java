d9556e3bb4d5ed797e1fb72c708f9e98856b22f3
wangcheng <veoxft@hotmail.com>
2017-09-30 16:55:21 +0800

    javaSE5以前版本好像基类能劫持接口，见泛型问题一节

---
+++ ch09-21/src/generics/ComparablePet.java
@@ -0,0 +1,11 @@
+package generics;
+
+/**
+ * Created by wangcheng  on 2017/9/30.
+ */
+public class ComparablePet implements Comparable<ComparablePet>{
+    @Override
+    public int compareTo(ComparablePet o) {
+        return 0;
+    }
+}
---
+++ ch09-21/src/generics/HijackedInterface.java
@@ -0,0 +1,13 @@
+package generics;
+
+/**
+ * Created by wangcheng  on 2017/9/30.
+ */
+class Cat extends ComparablePet implements Comparable<Cat>{
+    @Override
+    public int compareTo(Cat arg) {
+        return 0;
+    }
+}
+public class HijackedInterface {
+}
