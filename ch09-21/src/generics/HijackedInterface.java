8390f13021537111c323fd4ac9446df84bb4a5b2
wangcheng <veoxft@hotmail.com>
2017-09-30 17:34:13 +0800

    基类劫持接口

--- ch09-21/src/generics/HijackedInterface.java
+++ ch09-21/src/generics/HijackedInterface.java
@@ -3,11 +3,11 @@ package generics;
 /**
  * Created by wangcheng  on 2017/9/30.
  */
-class Cat extends ComparablePet implements Comparable<Cat>{
-    @Override
-    public int compareTo(Cat arg) {
-        return 0;
-    }
-}
+//class Cat extends ComparablePet implements Comparable<Cat>{
+//    @Override
+//    public int compareTo(Cat arg) {
+//        return 0;
+//    }
+//}
 public class HijackedInterface {
 }
