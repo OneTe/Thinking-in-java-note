eb57241d5d276ecec0d35c226d5efa647be7048d
wangcheng <veoxft@hotmail.com>
2017-12-08 15:34:23 +0800

    视图缓冲器

---
+++ ch09-21/src/io/Endians.java
@@ -0,0 +1,25 @@
+package io;
+//Endian differences and data storage.
+/**
+ * Created by wangcheng  on 2017/12/8.
+ */
+import java.nio.*;
+import java.util.Arrays;
+
+public class Endians {
+    public static void main(String[] args){
+        ByteBuffer bb = ByteBuffer.wrap(new byte[12]);
+        bb.asCharBuffer().put("abcdef");
+        System.out.println(Arrays.toString(bb.array()));
+        bb.rewind();
+        bb.order(ByteOrder.BIG_ENDIAN);
+        bb.asCharBuffer().put("abcdef");
+        System.out.println(Arrays.toString(bb.array()));
+        bb.rewind();
+
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        bb.asCharBuffer().put("abcdef");
+        System.out.println(Arrays.toString(bb.array()));
+        bb.rewind();
+    }
+}
