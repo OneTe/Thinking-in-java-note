c6f15f8be18ec0aacc99a025ee9e0cea67297ba6
wangcheng <veoxft@hotmail.com>
2017-12-22 11:14:57 +0800

    常量相关方法

--- ch09-21/src/enumerated/E11_VendingMachine3.java
+++ ch09-21/src/enumerated/E11_VendingMachine3.java
@@ -12,6 +12,8 @@ package enumerated;
  ***********************************************/
 
 import jdk.internal.util.xml.impl.Input;
+import net.mindview.util.Generator;
+import net.mindview.util.TextFile;
 
 import java.util.*;
 
@@ -105,4 +107,119 @@ enum Category2 {
 }
 
 public class E11_VendingMachine3 {
+    private static State state = State.RESTING;
+    private static int amount = 0;
+    private static ExtInput selection = null;
+    enum StateDuration { TRANSIENT } // Tagging enum
+    enum State {
+        RESTING {
+            void next(ExtInput input) {
+                switch(Category2.categorize(input.input)) {
+                    case MONEY:
+                        amount += input.amount();
+                        state = ADDING_MONEY;
+                        break;
+                    case SHUT_DOWN:
+                        state = TERMINAL;
+                    default: }
+            } },
+        ADDING_MONEY {
+            void next(ExtInput input) {
+                switch(Category2.categorize(input.input)) {
+                    case MONEY:
+                        amount += input.amount();
+                        break;
+                    case ITEM_SELECTION:
+                        selection = input;
+                        if(amount < selection.amount())
+                            System.out.println("Insufficient money for " + selection);
+                        else state = DISPENSING;
+                        break;
+                    case QUIT_TRANSACTION:
+                        state = GIVING_CHANGE;
+                        break;
+                    case SHUT_DOWN:
+                        state = TERMINAL;
+                    default: }
+            } },
+        DISPENSING(StateDuration.TRANSIENT) {
+            void next() {
+                System.out.println("here is your " + selection);
+                amount -= selection.amount();
+                state = GIVING_CHANGE;
+            } },
+        GIVING_CHANGE(StateDuration.TRANSIENT) {
+            void next() {
+                if(amount > 0) {
+                    System.out.println("Your change: " + amount);
+                    amount = 0;
+                }
+                state = RESTING;
+            }
+        },
+        TERMINAL { void output() { System.out.println("Halted"); } };
+        private boolean isTransient = false;
+        State() {}
+        State(StateDuration trans) { isTransient = true; }
+        void next(ExtInput input) {
+            throw new RuntimeException("Only call " +
+                    "next(ExtInput input) for non-transient states");
+        }
+        void next() {
+            throw new RuntimeException("Only call next() for " +
+                    "StateDuration.TRANSIENT states");
+        }
+        void output() { System.out.println(amount); }
+    }
+    static void run(Generator<ExtInput> gen) {
+        while(state != State.TERMINAL) {
+            state.next(gen.next());
+            while(state.isTransient)
+                state.next();
+            state.output();
+        } }
+    public static void main(String[] args) {
+        if(args.length < 1) {
+            System.err.println(
+                    "The vended items data file is not given!");
+            return; }
+        Generator<ExtInput> gen;
+        if(args.length == 2)
+            gen = new FileExtInputGenerator(args[1]);
+        else
+            gen = new RandomExtInputGenerator();
+        // Parse the vended items data file
+        for(String data : new TextFile(args[0], ";"))
+            VendedItem.addItem(VendedItem.parse(data.trim()));
+        run(gen);
+    } }
+// For a basic sanity check:
+class RandomExtInputGenerator
+        implements Generator<ExtInput> {
+    public ExtInput next() {
+        return new ExtInput(Input2.randomSelection(),
+                VendedItem.randomSelection());
+    }
+}
+// Create Inputs from a file of ';'-separated strings:
+class FileExtInputGenerator implements Generator<ExtInput> {
+    private Iterator<String> input;
+    public FileExtInputGenerator(String fileName) {
+        input = new TextFile(fileName, ";").iterator();
+    }
+    public ExtInput next() {
+        if(!input.hasNext())
+            return null;
+        String s = input.next().trim();
+        try {
+            return
+                    new ExtInput(Enum.valueOf(Input2.class,s), null);
+        } catch(IllegalArgumentException e) {
+            // B plan: probably a vended item...
+            VendedItem item = VendedItem.lookup(s);
+            if(item != null)
+                return new ExtInput(Input2.VENDED_ITEM, item);
+            throw e;  // Rethrow the catched exception
+        } }
+
 }
