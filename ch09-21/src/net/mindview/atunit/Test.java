7c76c877608e64038ee05ef46563c5270bbadb75
wangcheng <veoxft@hotmail.com>
2017-12-22 16:43:23 +0800

    注解

---
+++ ch09-21/src/net/mindview/atunit/Test.java
@@ -0,0 +1,10 @@
+package net.mindview.atunit;
+
+/**
+ * Created by wangcheng  on 2017/12/22.
+ */
+import java.lang.annotation.*;
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Test {
+}
