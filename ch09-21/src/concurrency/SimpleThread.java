1e8ba8b79ea3cd755b88dced0737441fbc47a546
wangcheng <veoxft@hotmail.com>
2018-01-05 18:25:53 +0800

    简单情况下直接继承Thread替换实现Runnable接口

---
+++ ch09-21/src/concurrency/SimpleThread.java
@@ -0,0 +1,30 @@
+package concurrency;
+//Inheriting directory from the Thread class
+/**
+ * Created by wangcheng  on 2018/1/5.
+ */
+public class SimpleThread extends Thread{
+    private int countDown = 5;
+    private static int threadCount = 0;
+    public SimpleThread(){
+        //Store the thread name
+        super(Integer.toString(++threadCount));
+        start();
+    }
+
+    @Override
+    public String toString() {
+        return "#" + getName() + "(" + countDown + "), ";
+    }
+    public void run(){
+        while (true){
+            System.out.println(this);
+            if(--countDown == 0)
+                return;
+        }
+    }
+    public static void main(String[] args){
+        for(int i = 0;i < 5;i++)
+            new SimpleThread();
+    }
+}
